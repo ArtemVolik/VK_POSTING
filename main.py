36f4f963f852362e9f679ae554d982c8e6476b05
artyom <art.volyk@gmail.com>
2020-11-19 15:56:53 +0200

    remove global variables from func

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from pathvalidate import sanitize_filename
 load_dotenv()
 
 params = {
-    'group_id': 200405671,
+    'group_id': os.getenv('group_id'),
     'access_token': os.getenv('access_token'),
     'v': 5.126
 }
@@ -48,7 +48,7 @@ def get_upload_url(params, proxies):
     return get_response(url, params=params, proxies=proxies)['response']['upload_url']
 
 
-def post_picture(upload_url, params, file_path, proxies=None):
+def post_picture(upload_url, file_path, params=None, proxies=None):
     with open(file_path, 'rb') as file:
         files = {
             'photo': file,
@@ -58,7 +58,7 @@ def post_picture(upload_url, params, file_path, proxies=None):
         return response.json()
 
 
-def save_picture_for_the_wall(params, posted_picture_data):
+def save_picture_for_the_wall(posted_picture_data, params=None, proxies=None):
     url = 'https://api.vk.com/method/photos.saveWallPhoto'
     upload_param = params.copy()
     upload_param['hash'] = posted_picture_data['hash']
@@ -68,7 +68,7 @@ def save_picture_for_the_wall(params, posted_picture_data):
     return response.json()
 
 
-def post_picture_on_the_wall(saved_picture_data):
+def post_picture_on_the_wall(saved_picture_data, params=None, proxies=None):
     photo_id = saved_picture_data['response'][0]['id']
     owner_id = saved_picture_data['response'][0]['owner_id']
     post_params = params.copy()
@@ -90,11 +90,10 @@ if __name__ == '__main__':
     comics = fetch_comics(comics_id)
     file_name = save_picture(comics)
     upload_url = get_upload_url(params, proxies)
-    posted_picture_data = post_picture(upload_url, params, file_name, proxies=proxies)
-    saved_picture_data = save_picture_for_the_wall(params, posted_picture_data)
-    print('вот принт')
+    posted_picture_data = post_picture(upload_url, file_name, params, proxies)
+    saved_picture_data = save_picture_for_the_wall(posted_picture_data, params, proxies)
     pprint(saved_picture_data)
-    post_picture_on_the_wall(saved_picture_data)
+    post_picture_on_the_wall(saved_picture_data, params, proxies)
     os.remove(file_name)
 
 
